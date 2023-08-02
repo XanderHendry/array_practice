f5146ce9175a88db84cc44e87ab05c3f35fb10cd
Xander Hendry <vhendry.coding@gmail.com>
2023-08-02 13:33:14 -0400

    Add index position demonstration

--- arrays.rb
+++ arrays.rb
@@ -11,3 +11,10 @@ age.push(7)
 # This code calls the push method on array age and will insert the integer 7 at the end of the array
 names.unshift("Xander")
 # This code calls the unshift method on array names and will insert the string "Xander" at the end of the array
+
+# Index Positions
+# Index positions are numbered positions in an array, starting with 0.
+names[2]  # This code will retrieve the value in the third index position, Fen, from the array names.
+age[0] # This code will retrieve the value in the first index position, 32, from the array age.
+floats[3] # This code will retrieve the value in the 4th index position, 0.05, from the array floats.
+

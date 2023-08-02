a0d5679282aca76ef9d66b19ece8ccafbd95dcca
Xander Hendry <vhendry.coding@gmail.com>
2023-08-02 13:20:17 -0400

    Add array methods and explanations

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,13 @@
-names = ["Xander", "Kaity", "Clover", "Fen", "Sadie"]
-age = ["32", "31", "1", "9", "7"]
-floats = ["1.4", "3.5", "6.9", "0.05"]
+names = ["Kaity", "Clover", "Fen", "Sadie"]
+age = [32, 31, 1, 9]
+floats = [1.4, 3.5, 6.9, 0.05]
 bool = ["true", "false", "false", "true"]
 
+bool.pop
+# This code calls the pop method on array bool and will retrieve and remove the value in the last index position af the array
+floats.shift  
+# This code calls the shift method on array floats and will retrieve and remove the value in the first index position of the array.
+age.push(7)
+# This code calls the push method on array age and will insert the integer 7 at the end of the array
+names.unshift("Xander")
+# This code calls the unshift method on array names and will insert the string "Xander" at the end of the array
